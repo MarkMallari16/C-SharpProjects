b5872a11caabebb2bb0d112a86dd26e63e9b623a
Mark Mallari <mallariftw01@gmail.com>
2023-09-13 23:01:14 +0800

    commit

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace NameSpace1
+{
+    public class MyClassOne
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Hello From My Class One");
+        }
+    }
+}
+namespace NameSpace2
+{
+    public class MyClassTwo
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Hello From My Class Two");
+        }
+    }
+}
+//main
+namespace NameSpaceDemo
+{
+    public class MyFirstProgram
+    {
+        public static void Main()
+        {
+            NameSpace1.MyClassOne obj1 = new NameSpace1.MyClassOne();
+            NameSpace2.MyClassTwo obj2 = new NameSpace2.MyClassTwo();
+
+            obj1.Greet();
+            obj2.Greet();
+        }
+    }
+}
